619df734dd978eb1377974c0979d67020aefc4e7
0429oonishi <oonishireon110@gmail.com>
2021-05-16 21:07:19 +0900

    first commit

---
+++ oonishiReonKadai6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  oonishiReonKadai6
+//
+//  Created by 大西玲音 on 2021/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
